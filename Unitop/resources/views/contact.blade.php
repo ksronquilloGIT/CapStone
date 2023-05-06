7f335e7bc7080bea66aba063f04828c2d9623258
ejayjayjayjayjay <vibarjosell@gmail.com>
2023-05-06 21:48:27 +0800

    add contact and services page

---
+++ Unitop/resources/views/contact.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+
+    <div class="bg-background"></div>
+    <div class="container py-5">
+        <div class="row py-5 g-3">
+
+            <div class="col-md-6 first_col ">
+                <h1 class="text-center mt-3">Contact Us</h1>
+                <form class="p-4 mt-5">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Enter your Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email ID</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Enter your massage</label>
+                        <textarea  type="text" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <button class="btn btn-primary">Send Now</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6 sec_col">
+                <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=550&amp;hl=en&amp;q=UNITOP metalwire&amp;t=k&amp;z=20&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://capcuttemplate.org/">Capcuttemplate.org</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:550px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:550px;}.gmap_iframe {height:550px!important;}</style></div>
+            </div>
+        </div>
+    </div>
+
+
+</x-layout>
---
+++ Unitop/resources/views/services.blade.php
@@ -0,0 +1,122 @@
+<x-layout>
+
+    <!-- SERVICES SECTION START -->
+
+   
+    <section id="gallery">
+       <div class="container">
+           <div class="row">
+               <div class="title text-center">
+                   <h1>Our Services</h1>
+               </div>
+           </div>
+         <div class="row">
+         <div class="col-lg-6 mb-6">
+         <div class="card">
+           <img src="{{URL('storage/Picture4.png')}}" alt="" class="card-img-top img-fluid" style="height: 276px;">
+           <div class="card-body text-center">
+             <h5 class="card-title">Metal Insert Gas Welding (MIG)</h5>
+             <p class="card-text lh-2">This style of welding is also referred to as Metal Inert Gas (MIG). It uses a shielding gas along the wire electrode, which heats up the two metals to be joined. This method requires a constant voltage and direct-current power source, and is the most common industrial welding process which includes plate and large bore pipe.</p>
+            <a href="" class="btn btn-outline-success btn-sm">Contact Us</a>
+           </div>
+          </div>
+         </div>
+         <div class="col-lg-6 mb-6">
+         <div class="card">
+           <img src="{{URL('storage/plasmaArc2.jpg')}}" alt="" class="card-img-top img-fluid" style="height: 276px;">
+           <div class="card-body text-center">
+             <h5 class="card-title">Plasma Arc Welding (PAW)</h5>
+             <p class="card-text lh-1">Plasma arc welding, or PAW, is similar to GTAW, but it uses a smaller arc and lower voltages which increase the precision of the weld. This precision and voltage allows the use of this process to assemble delicate and heat sensitive devices, such a microchips and medical devices.The torch can also achieve very high temperatures in small spaces. Gas is pressurized inside the wand, creating plasma. The plasma is then ionized, making it electrically conductive. This allows the arc to be created, producing incredibly high temperatures that can melt the base metals. This allows plasma arc welding to be performed with no filler metal, a similarity it shares with TIG welding.</p>
+            <a href="" class="btn btn-outline-success btn-sm">Contact Us</a>
+           </div>
+          </div>
+         </div>
+       </div>
+     </div>
+     </section>
+
+   <!-- SERVICES SECTION END -->
+
+   <div class="container gallery-container">
+
+       <h1>SAMPLE FINISH PRODUCTS</h1>
+   
+       <p class="page-description text-center"></p>
+       
+       <div class="tz-gallery">
+   
+           <div class="row">
+   
+               <div class="col-sm-6 col-md-4 text-center">
+                   <div class="thumbnail">
+                       <a class="lightbox" href="{{URL('storage/bicycleBasket.jpg')}}">
+                           <img class="img-fluid" src="{{URL('storage/bicycleBasket.jpg')}}" alt="">
+                       </a>
+                       <div class="caption">
+                           <h3>BB 131</h3>
+                           <p>Bicycle Basket Brace</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-sm-6 col-md-4 text-center">
+                   <div class="thumbnail">
+                       <a class="lightbox" href="{{URL('storage/apCabinetBasket.jpg')}}">
+                           <img src="{{URL('storage/apCabinetBasket.jpg')}}" alt="Bridge">
+                       </a>
+                       <div class="caption">
+                           <h3>KC-010</h3>
+                           <p>AP CABINET BASKET</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-sm-6 col-md-4 text-center">
+                   <div class="thumbnail">
+                       <a class="lightbox" href="{{URL('storage/showerCaddy.jpg')}}">
+                           <img src="{{URL('storage/showerCaddy.jpg')}}" alt="Tuneel">
+                       </a>
+                       <div class="caption">
+                           <h3>BR-124</h3>
+                           <p>SHOWER CADDY OCTAGON</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-sm-6 col-md-4 text-center">
+                   <div class="thumbnail">
+                       <a class="lightbox" href="{{URL('storage/doubleDeck.jpg')}}">
+                           <img src="{{URL('storage/doubleDeck.jpg')}}" alt="Coast">
+                       </a>
+                       <div class="caption">
+                           <h3>DD-202</h3>
+                           <p>STEEL BED DOUBLE DECK</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-sm-6 col-md-4 text-center">
+                   <div class="thumbnail">
+                       <a class="lightbox" href="{{URL('storage/hangingPlate.jpg')}}">
+                           <img src="{{URL('storage/hangingPlate.jpg')}}" alt="Rails">
+                       </a>
+                       <div class="caption">
+                           <h3>KU-114 & KU-113</h3>
+                           <p>HANGING PLATE RACK</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-sm-6 col-md-4 text-center">
+                   <div class="thumbnail">
+                       <a class="lightbox" href="{{URL('storage/openBasket.jpg')}}">
+                           <img src="{{URL('storage/openBasket.jpg')}}" alt="Traffic">
+                       </a>
+                       <div class="caption">
+                           <h3>O-45</h3>
+                           <p>OPEN END BASKET</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+       </div>
+   
+   </div>
+
+</x-layout>
